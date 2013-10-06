466384873492c6d98709b309ecbd26a33e73d193
James Andariese <james.andariese@locationlabs.com>
2013-10-06 15:17:16 -0700

    Initial import

---
+++ pebblemon.el
@@ -0,0 +1,39 @@
+;;; pebblemon.el --- PebbLemon integration for Emacs
+
+;; Copyright (C) 2013 James Andariese
+
+;; Author: James Andariese <james@strudelline.net>
+;; Version: 1.1
+;; Package-Requires: ((json "0.0"))
+;; Keywords: pebble jabber
+
+;;; Commentary:
+
+;; This package provides a function to send messages to your Pebble
+;; through the PebbLemon service on Google App Engine
+
+(require 'json)
+
+;;;###autoload
+(defun pebblemon-send (title msg)
+  (interactive "sTitle: \nsMessage: ")
+  (start-process "jabber.el.pebble.strudelline.net" nil
+		 "curl"
+		 "-d" (json-encode `((auth . ,pebblemon-auth)
+				     (title . ,title)
+				     (message . ,msg)))
+		 "https://pebblemon.appspot.com/send"))
+
+;;;###autoload
+(defgroup pebblemon nil
+  "PebbLemon integration options"
+  :group 'applications)
+
+;;;###autoload
+(defcustom pebblemon-auth
+  ""
+  "PebbLemon auth token (usually a UUID)"
+  :type '(string)
+  :group 'pebblemon)
+
+;;; pebblemon.el ends here
